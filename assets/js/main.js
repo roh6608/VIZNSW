e3148f20b55880cb75a8d335005cc9d001d2a2e9
roh6608 <72960901+roh6608@users.noreply.github.com>
2021-01-09 20:41:26 +1100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,27 @@
 			alignment: 'center'
 		});
 
+	// Nav.
 
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
 
 })(jQuery);
